10dbec6819ee4ea9280c43e197aca890a495eabc
PANHA TENG <tengpanha35@gmail.com>
2023-05-20 19:05:53 +0700

    complete jwt implement with role permission

--- src/main/java/com/example/apidemo/ApiDemoApplication.java
+++ src/main/java/com/example/apidemo/ApiDemoApplication.java
@@ -1,13 +1,50 @@
 package com.example.apidemo;
 
+import com.example.apidemo.security.auth.AuthenticationService;
+import com.example.apidemo.security.auth.RegisterRequest;
+import static  com.example.apidemo.utils.Role.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiDemoApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@gmail.com")
+                    .password("admin")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+            var manager = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("manager@gmail.com")
+                    .password("manager")
+                    .role(MANAGER)
+                    .build();
+            System.out.println("Manager token: " + service.register(manager).getAccessToken());
+            var user = RegisterRequest.builder()
+                    .firstname("User")
+                    .lastname("User")
+                    .email("user@gmail.com")
+                    .password("user")
+                    .role(USER)
+                    .build();
+            System.out.println("User token: " + service.register(user).getAccessToken());
+        };
     }
 
 }
