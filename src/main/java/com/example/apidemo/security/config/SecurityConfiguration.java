de5033dfb563af56de4d241432a422748c3868a1
PANHA TENG <tengpanha35@gmail.com>
2023-06-25 19:10:51 +0700

    doploy to render

--- src/main/java/com/example/apidemo/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/apidemo/security/config/SecurityConfiguration.java
@@ -30,8 +30,6 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf()
-        .disable()
         .authorizeHttpRequests()
         .requestMatchers(
                 "/api/v1/auth/**",
